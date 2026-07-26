307e5cf8eeb6e7b2188fc5df019cbf69eb7e2fe9
kaaxberox <kaaxberox@gmail.com>
2026-07-26 11:53:30 +0800

    Add farmList.js

---
+++ farmList.js
@@ -0,0 +1,10 @@
+// 5 Farm yang kamu tandai
+module.exports = [
+  { name: "rafi", id: "955069302116900" },
+  { name: "ayun", id: "8816103743677689" },
+  { name: "hani", id: "1745841803328864" },
+  { name: "thohir", id: "3503211691330497" },
+  { name: "chanra", id: "2596473604955719" }
+];
+
+
